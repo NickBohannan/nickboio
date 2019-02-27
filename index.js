520a210ecf0a821be2d0d9389524bcc918fe4bd2
Nick Bohannan <nbohannan@gmail.com>
2019-02-26 22:09:13 -0600

    nodestatic

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
-const express = require('express')
-const app = express()
-const path = require('path')
+var static = require('node-static');
+var file = new static.Server();
 
-const PORT = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/dist/index.html'))
-})
-
-
-app.listen(PORT, () => {
-    console.log(`Our app is running on port ${PORT}`);
-});
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        file.serve(request, response);
+    }).resume();
+}).listen(process.env.PORT || 3000);
