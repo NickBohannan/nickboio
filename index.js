4c5962241cb48e6e29f9cc3d9dd5bb7eeb59f096
Nick Bohannan <nbohannan@gmail.com>
2019-02-26 21:45:05 -0600

    use env port

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
 
+const PORT = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
     res.sendFile('index.html')
 })
 
-app.listen(8000)
+
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${PORT}`);
+});
